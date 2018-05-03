c16925b7affce9d03caffa0681c10f66821086e6
MichaelRavid <Michael.Ravid@hotmail.com>
2018-05-02 20:35:21 -0400

    buz-roy-1

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,1 @@
-Object.prototype.each = function(f){
-    var l = this.length;
-    for(var i=0;i<l;i++)
-        {
-            try {
-                this[i].eachCall = f;
-                this[i].eachCall(this[i]);
-            } catch(e){ throw e; }
-        }
-};
-
-document.getElementsByTagName('iframe').each(function(){this.src=this.src;});
+// alert("This is a test");
